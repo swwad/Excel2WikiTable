a58c4946bc2836dd0da69c2f5ea60c93ea925ae6
Darren <swwad1@gmail.com>
2020-04-06 19:25:50 +0800

    first commit

---
+++ Excel2WikiTable/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Excel2WikiTable
+{
+    partial class Excel2WikiTable
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParse = new System.Windows.Forms.Button();
+            this.btnSelectDirectory = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnParse
+            // 
+            this.btnParse.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnParse.Location = new System.Drawing.Point(376, 319);
+            this.btnParse.Margin = new System.Windows.Forms.Padding(2);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(90, 32);
+            this.btnParse.TabIndex = 0;
+            this.btnParse.Text = "Parse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // btnSelectDirectory
+            // 
+            this.btnSelectDirectory.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSelectDirectory.Location = new System.Drawing.Point(330, 9);
+            this.btnSelectDirectory.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSelectDirectory.Name = "btnSelectDirectory";
+            this.btnSelectDirectory.Size = new System.Drawing.Size(135, 32);
+            this.btnSelectDirectory.TabIndex = 3;
+            this.btnSelectDirectory.Text = "Select Directory";
+            this.btnSelectDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectDirectory.Click += new System.EventHandler(this.btnSelectDirectory_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Path : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(13, 46);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(453, 269);
+            this.textBox2.TabIndex = 6;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // Excel2WikiTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 354);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSelectDirectory);
+            this.Controls.Add(this.btnParse);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Excel2WikiTable";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Excel2WikiTable_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.Button btnSelectDirectory;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Excel2WikiTable/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Excel2WikiTable
+{
+    enum emCellStatus { Nonne, UniqueCell, MergeCellHeader, MergeCellBody };
+
+    public partial class Excel2WikiTable : Form
+    {
+
+        object missingValue = System.Reflection.Missing.Value;
+        Excel.Application excelApp = null;
+        float dpiX = 0;
+        float dpiY = 0;
+
+        public Excel2WikiTable()
+        {
+            InitializeComponent();
+            Graphics graphics = this.CreateGraphics();
+            dpiX = graphics.DpiX;
+            dpiY = graphics.DpiY;
+        }
+
+        private bool MakeHtmlTable(string outputFileName, List<List<SimpleExcelCell>> processDataTable)
+        {
+            try
+            {
+                using (StreamWriter outputFile = new StreamWriter(outputFileName))
+                {
+                    outputFile.WriteLine("<table>");
+                    foreach (List<SimpleExcelCell> row in processDataTable)
+                    {
+                        outputFile.Write("\t<tr>");
+                        foreach (SimpleExcelCell column in row)
+                        {
+                            System.Windows.Forms.Application.DoEvents();
+                            outputFile.Write("<td width=\"" + column.CellWidthPixels + "\">");
+                            outputFile.Write(column.CellValue);
+                            outputFile.Write("</td>");
+                        }
+                        outputFile.WriteLine("</tr>");
+                    }
+                    outputFile.WriteLine("</table>");
+                    outputFile.Flush();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private List<List<SimpleExcelCell>> ProcessWorkSheet(Worksheet worksheet)
+        {
+
+            int usedColumnsCount = worksheet.UsedRange.Cells.Columns.Count; // Total columns count
+            int usedRowsCount = worksheet.UsedRange.Cells.Rows.Count; // Total rows count
+            int[] columnWidrhPixel = new int[usedColumnsCount];
+
+            #region Get every column width and save it in array.
+            for (int i = 1; i <= usedRowsCount; i++)
+            {
+                int thisRowColumnsCount = worksheet.UsedRange.Rows[i].Columns.Count;
+                if (thisRowColumnsCount < usedColumnsCount) // If thisRowColumnsCount less then usedColumnsCount, this row has merge column
+                {
+                    continue;
+                }
+                for (int j = 1; j <= thisRowColumnsCount; j++)
+                {
+                    columnWidrhPixel[j - 1] = Points2Pixels(worksheet.Cells[i, j].Width);
+                }
+                break;  // If get all columns width, break out this loop
+            }
+            #endregion
+
+            #region Get excel value
+            List<List<SimpleExcelCell>> rowList = new List<List<SimpleExcelCell>>();
+            for (int i = 1; i <= usedRowsCount; i++)
+            {
+                List<SimpleExcelCell> columnList = new List<SimpleExcelCell>();
+                int columnsCount = worksheet.UsedRange.Rows[i].Columns.Count;
+                int widthTailIndex = usedColumnsCount;
+                int currentCellIndex = 0;
+
+                for (int j = 1; j <= columnsCount; j++)
+                {
+                    System.Windows.Forms.Application.DoEvents();
+                    // Check excel range(cell) merge and value status.
+                    emCellStatus cellStatus = ConvertCellStatus(worksheet.Cells[i, j], j);
+                    if (cellStatus == emCellStatus.UniqueCell || cellStatus == emCellStatus.MergeCellHeader)
+                    {
+                        string currentCellName = worksheet.Cells[i, j].Address.ToString();
+                        string currentCellValue = worksheet.Cells[i, j].Value2 == null ? "" : worksheet.Cells[i, j].Value2.ToString();
+                        columnList.Add(new SimpleExcelCell(currentCellName, currentCellValue, cellStatus));
+                        columnList[currentCellIndex].CellWidthPixels = columnWidrhPixel[j - 1];
+                        currentCellIndex++;
+                    }
+                    else if (cellStatus == emCellStatus.MergeCellBody)
+                    {
+                        columnList[currentCellIndex - 1].CellWidthPixels += columnWidrhPixel[j - 1];
+                        continue;
+                    }
+                    else
+                    {
+                        throw new Exception("Error:emCellStatus = " + cellStatus.ToString());
+                    }
+                }
+                rowList.Add(columnList);
+            }
+            #endregion
+            return rowList;
+        }
+
+        private Worksheet GetExcelWorkSheet(string excelFileName, int workSheetNumber)
+        {
+            try
+            {
+                excelApp = new Excel.Application();
+                if (excelApp == null)
+                {
+                    throw new ArgumentNullException("excelApp Open Failed");
+                }
+                else
+                {
+                    excelApp.Visible = false;
+                    excelApp.UserControl = true;
+                    Workbook excelWorkBook = excelApp.Application.Workbooks.Open(excelFileName, missingValue, true, missingValue,
+                        missingValue, missingValue, missingValue, missingValue, missingValue, true, missingValue,
+                        missingValue, missingValue, missingValue, missingValue);
+                    return (Worksheet)excelWorkBook.Worksheets.get_Item(workSheetNumber);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                excelApp.Quit();
+                excelApp = null;
+                return null;
+            }
+        }
+
+        private void KillExcelProcess()
+        {
+            Process[] procs = Process.GetProcessesByName("excel");
+            foreach (Process pro in procs)
+            {
+                pro.Kill(); //Kill process is a easy way to do
+            }
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+
+            string[] exceFilesList = StackBasedIteration.TraverseTree(textBox1.Text);
+
+
+            for (int i = 0; i < exceFilesList.Length; i++)
+            {
+                string excelFile = exceFilesList[i];
+                if (File.Exists(System.IO.Path.ChangeExtension(excelFile, "md")))
+                {
+                    textBox2.AppendText(System.IO.Path.ChangeExtension(excelFile, "md") + "...already exist.\r\n");
+                }
+                Worksheet worksheet = GetExcelWorkSheet(excelFile, 1);
+                List<List<SimpleExcelCell>> processDataTable = ProcessWorkSheet(worksheet);
+                MakeHtmlTable(System.IO.Path.ChangeExtension(excelFile, "md"), processDataTable);
+                textBox2.AppendText(System.IO.Path.ChangeExtension(excelFile, "md") + "...OK!\r\n");
+                if (i % 10 == 0)
+                {
+                    KillExcelProcess();
+                }
+            }
+            MessageBox.Show("Done!");
+
+        }
+
+        //private int Alphabet2Number(string strAlphabet)
+        //{
+        //    string strNumber = string.Empty;
+        //    foreach (char charAlphabet in strAlphabet)
+        //    {
+        //        int index = (char.ToUpper(charAlphabet) - 'A') + 1; // Alphabet index start from 1
+        //        strNumber += index;
+        //    }
+        //    return Convert.ToInt32(strNumber);
+        //}
+
+        private int Points2Pixels(double iPoint)
+        {
+            return Convert.ToInt32(iPoint * dpiX / 72);
+        }
+
+        private emCellStatus ConvertCellStatus(Range range, int xAddress)
+        {
+            bool bMergeCell = range.MergeCells;
+            bool bHasValue = range.Value2 == null ? false : true;
+            if (bMergeCell)
+            {
+                if (bHasValue)
+                {
+                    return emCellStatus.MergeCellHeader;
+                }
+                else
+                {
+                    if (xAddress == 1)  // If address x is the first column, it should be cell header
+                    {
+                        return emCellStatus.MergeCellHeader;
+                    }
+                    else
+                    {
+                        return emCellStatus.MergeCellBody;
+                    }
+                }
+            }
+            else
+            {
+                return emCellStatus.UniqueCell;
+            }
+        }
+
+        private void btnSelectDirectory_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void Excel2WikiTable_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            KillExcelProcess();
+        }
+    }
+
+    class SimpleExcelCell   // Make simple data struct, let us read data clearly.
+    {
+        public string CellName = string.Empty;
+        public string CellValue = string.Empty;
+        public int CellWidthPixels = 0;
+        public emCellStatus CellStatus = emCellStatus.Nonne;
+
+        public SimpleExcelCell(string strName, string strValue, emCellStatus cellStatus)
+        {
+            this.CellName = strName;
+            this.CellValue = strValue;
+            this.CellStatus = cellStatus;
+        }
+    }
+
+    class StackBasedIteration
+    {
+        public static string[] TraverseTree(string root)
+        {
+            List<string> excelFilesList = new List<string>();
+            excelFilesList.Clear();
+
+            // Data structure to hold names of subfolders to be
+            // examined for files.
+            Stack<string> dirs = new Stack<string>(20);
+
+            if (!System.IO.Directory.Exists(root))
+            {
+                throw new ArgumentException();
+            }
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs;
+                try
+                {
+                    subDirs = System.IO.Directory.GetDirectories(currentDir);
+                }
+                // An UnauthorizedAccessException exception will be thrown if we do not have
+                // discovery permission on a folder or file. It may or may not be acceptable 
+                // to ignore the exception and continue enumerating the remaining files and 
+                // folders. It is also possible (but unlikely) that a DirectoryNotFound exception 
+                // will be raised. This will happen if currentDir has been deleted by
+                // another application or thread after our call to Directory.Exists. The 
+                // choice of which exceptions to catch depends entirely on the specific task 
+                // you are intending to perform and also on how much you know with certainty 
+                // about the systems on which this code will run.
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                string[] files = null;
+                try
+                {
+                    files = System.IO.Directory.GetFiles(currentDir, "*.xls");
+                    excelFilesList.AddRange(files);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                // Push the subdirectories onto the stack for traversal.
+                // This could also be done before handing the files.
+                foreach (string str in subDirs)
+                    dirs.Push(str);
+            }
+            return excelFilesList.ToArray();
+        }
+    }
+}
